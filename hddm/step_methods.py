7e0ca276fdf0c6a896ac193bf9035728c3a96c90
Imri Sofer <imrisofer@gmail.com>
2012-01-13 12:47:47 -0500

    commented out GammaPriorTruncNormal

--- hddm/step_methods.py
+++ hddm/step_methods.py
@@ -1,4 +1,5 @@
 import pymc as pm
+import numpy as np
 
 class NormalPriorNormal(pm.Gibbs):
     """
@@ -29,25 +30,25 @@ class NormalPriorNormal(pm.Gibbs):
         self.stochastic.value = np.random.randn()/tau_prime + mu_prime
     
     
-class GammaPriorTruncNormal(pm.Metropolis):
-
-    linear_OK = True
-    child_class = pm.TruncatedNormal
-    parent_label = 'tau'
-    target_class = pm.Gamma
-
-    def __init(selfself, stochastic, *args, **kwargs):
-        pm.Metropolis.__init__(self, stochastic, *args, **kwargs)
-        self.stochastic = stochastic
-        
-        #get boundaries
-        ab = np.unique(array([(x.a, x.b) for x in self.children]))
-        assert len(ab)==1, "children nodes should have the same boundaries"        
-        self.a, self.b = ab[0], ab[1]
-            
-        self.mu_node = self.children[0].parents['mu']
-    def step(self):
-        mu = self.mu_node.value
-        t_sigma = 1./sqrt(self.stochastic.value)
-        if norm.pdf(self.a, mu, )
+#class GammaPriorTruncNormal(pm.Metropolis):
+#
+#    linear_OK = True
+#    child_class = pm.TruncatedNormal
+#    parent_label = 'tau'
+#    target_class = pm.Gamma
+#
+#    def __init(selfself, stochastic, *args, **kwargs):
+#        pm.Metropolis.__init__(self, stochastic, *args, **kwargs)
+#        self.stochastic = stochastic
+#        
+#        #get boundaries
+#        ab = np.unique(array([(x.a, x.b) for x in self.children]))
+#        assert len(ab)==1, "children nodes should have the same boundaries"        
+#        self.a, self.b = ab[0], ab[1]
+#            
+#        self.mu_node = self.children[0].parents['mu']
+#    def step(self):
+#        mu = self.mu_node.value
+#        t_sigma = 1./sqrt(self.stochastic.value)
+#        if norm.pdf(self.a, mu, )
         
